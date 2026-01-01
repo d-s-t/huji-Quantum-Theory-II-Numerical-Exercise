593f0dd6b9e345868ef97f1d1eef08af0eec2a9e
d-s-t <davidst360@gmail.com>
2026-01-01 16:12:34 +0200

    feat: implement second order task for hydrogen atom and update convergence rates table

--- main.py
+++ main.py
@@ -175,8 +175,8 @@ def first_order_task(folder_name: str = 'first_order'):
     residual_error_nm_l0_regular = np.abs(ground_state_energies_nm_l0_regular - exact_energy_l0)
 
     residual_error_fig = go.Figure([
-        go.Scatter(x=N_values, y=residual_error_nm_l0_first_order, mode='markers+lines', name=f'NM l=0 (First Order)'),
-        go.Scatter(x=N_values, y=residual_error_nm_l0_regular, mode='markers+lines', name=f'NM l=0 (Regular)')
+        go.Scatter(x=N_values, y=residual_error_nm_l0_first_order, mode='markers+lines', name=f'NM (First Order)'),
+        go.Scatter(x=N_values, y=residual_error_nm_l0_regular, mode='markers+lines', name=f'NM (Regular)')
     ])\
                 .update_xaxes(title_text='N', type='log')\
                 .update_yaxes(title_text='Residual Error (a.u.)', type='log', showexponent='all', exponentformat='power')\
@@ -194,27 +194,41 @@ def first_order_task(folder_name: str = 'first_order'):
 def second_order_task(folder_name: str = 'second_order'):
     """
     do the same as the first order task but use numerov_method_coulomb_l0_2nd_order
+    compare the results to the first order method and to the regular numerov method
     """
     makedirs(path.join('results', folder_name), exist_ok=True)
     N_values = [80, 120, 240, 360, 480, 600]
     R = 50
     Z = 1 # For hydrogen atom
-    ground_state_energies_nm_l0_second_order = np.empty_like(N_values, dtype=float)
-    for i, N in enumerate(N_values):
-        evals_nm = numerov_method_coulomb_l0_2nd_order(Z, R, N, eigvals_only=True)
-        ground_state_energies_nm_l0_second_order[i] = evals_nm[0]
+    from potential_functions import hydrogen_atom_potential as V
+    
+    ground_state_energies_nm_l0 = np.fromiter((numerov_method_radial(0, V, np.linspace(0, R, N + 1)[1:], eigvals_only=True)[0] for N in N_values), dtype=float)
+    ground_state_energies_nm_l0_first_order = np.fromiter((numerov_method_coulomb_l0_1st_order(Z, R, N, eigvals_only=True)[0] for N in N_values), dtype=float)
+    ground_state_energies_nm_l0_second_order = np.fromiter((numerov_method_coulomb_l0_2nd_order(Z, R, N, eigvals_only=True)[0] for N in N_values), dtype=float)
+
     exact_energy_l0 = -0.5 # for l=0, n=1, E = -Z^2/(2n^2) = -1^2/(2*1^2) = -0.5 a.u.
+    residual_error_nm_l0 = np.abs(ground_state_energies_nm_l0 - exact_energy_l0)
+    residual_error_nm_l0_first_order = np.abs(ground_state_energies_nm_l0_first_order - exact_energy_l0)
     residual_error_nm_l0_second_order = np.abs(ground_state_energies_nm_l0_second_order - exact_energy_l0)  
     residual_error_fig = go.Figure([
-        go.Scatter(x=N_values, y=residual_error_nm_l0_second_order, mode='markers+lines', name=f'NM l=0 (Second Order)')
+        go.Scatter(x=N_values, y=residual_error_nm_l0_second_order, mode='markers+lines', name=f'NM (Second Order)'),
+        go.Scatter(x=N_values, y=residual_error_nm_l0_first_order, mode='markers+lines', name=f'NM (First Order)'),
+        go.Scatter(x=N_values, y=residual_error_nm_l0, mode='markers+lines', name=f'NM (Regular)')
     ])\
                 .update_xaxes(title_text='N', type='log')\
                 .update_yaxes(title_text='Residual Error (a.u.)', type='log', showexponent='all', exponentformat='power')\
                 .update_layout(legend=dict(title='Method and l'))
     plotly_export(residual_error_fig, path.join(folder_name, 'residual_error'))
+    (_, q_nm), _ = curve_fit(error_func, N_values, residual_error_nm_l0)
+    (_, q_nm_o1), _ = curve_fit(error_func, N_values, residual_error_nm_l0_first_order)
     (_, q_nm_o2), _ = curve_fit(error_func, N_values, residual_error_nm_l0_second_order)
-    with open(path.join('results' ,folder_name, 'q_nm_o2.tex'), 'w') as f:
-        f.write(f'$q = {q_nm_o2:.4f}$')
+    with open(path.join('results' ,folder_name, 'convergence_rates.tex'), 'w') as f:
+        f.write(r'\begin{tabular}{c c}' + '\n')
+        # f.write(r'\hline' + '\n')
+        f.write(r'Method & $q$ \\' + '\n')
+        f.write(f'שיטת נומרוב & ${q_nm:.6g}$ \\\\' + '\n')
+        f.write(f'שיטת נומרוב (סדר ראשון) & ${q_nm_o1:.6g}$ \\\\' + '\n')
+        f.write(f'שיטת נומרוב (סדר שני) & ${q_nm_o2:.6g}$ \\\\' + '\n')
 
 
 
