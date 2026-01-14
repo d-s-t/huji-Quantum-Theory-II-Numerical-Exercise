e60c3ad992338c5ae345872848a41bd342c4af4b
d-s-t <davidst360@gmail.com>
2026-01-14 14:00:41 +0200

    Add script to generate LaTeX file list from Python files in the directory

---
+++ list_py_files_to_latex.py
@@ -0,0 +1,13 @@
+from glob import glob
+
+# get all the py file in the current directory
+py_files = glob("*.py")
+
+# filter out this file (get the name from variable __file__)
+py_files = [f for f in py_files if f != __file__]
+
+# create a latex document listing the files named file_list.tex
+with open("file_list.tex", "w") as f:
+    f.write(r"\newcommand{\filelist}{" + "\n")
+    f.write(",\n".join(py_files) + "\n")
+    f.write("}\n")
